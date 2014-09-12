303da5d89476f1f5b5764f3e2179d39a1bd4b162
Julien Muetton <themouette@gmail.com>
2014-09-12 18:09:29 +0200

    Closures are not required to isolate module context in NodeJS

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 'use strict';
 
-(function () {
-  var mainApp = require('./app/main')(),
-      server  = require('http').Server(mainApp),
-      port = process.env.PORT || require('./app/config').server.port;
+var mainApp = require('./app/main')(),
+    server  = require('http').Server(mainApp),
+    port = process.env.PORT || require('./app/config').server.port;
 
-  require('./app/ws')(server);
+require('./app/ws')(server);
 
-  server.listen(port);
-  console.log('application listening on port %s', port);
-})();
+server.listen(port);
+console.log('application listening on port %s', port);
