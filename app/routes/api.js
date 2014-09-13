ce0a11a8d4b6212d74ccbe5bc04e428413580550
Alexis Mineaud <alexis.mineaud@gmail.com>
2014-09-13 10:24:15 +0200

    Keep the same pattern to implement modules.

--- app/routes/api.js
+++ app/routes/api.js
@@ -1,40 +1,63 @@
-'use strict';
+module.exports = (function () {
+  'use strict';
 
-var apiRouter = require('express').Router();
+  var apiRouter = require('express').Router();
 
-function notImplementedYet(res) {
-  res.json({ message: 'Not implemented yet' });
-}
+  function notImplementedYet(res) {
+    res.json({ message: 'Not implemented yet' });
+  }
 
-function index(req, res) {
-  res.json({
-    message: 'Welcome to the procKr api!\nAPI is reachable.'
-  });
-}
+  function index(req, res) {
+    res.json({
+      message: 'Welcome to the procKr api!\nAPI is reachable.'
+    });
+  }
 
-function listProxies(req, res) {
-  notImplementedYet(res);
-}
-function getProxy(req, res) {
-  notImplementedYet(res);
-}
-function createProxy(req, res) {
-  notImplementedYet(res);
-}
-function updateProxy(req, res) {
-  notImplementedYet(res);
-}
-function removeProxy(req, res) {
-  notImplementedYet(res);
-}
+  /**
+   * @FIXME
+   */
+  function listProxies(req, res) {
+    notImplementedYet(res);
+  }
 
-apiRouter.route('/').get(index);
-apiRouter.route('/proxies')
-  .get(listProxies)
-  .post(createProxy);
-apiRouter.route('/proxies/:id')
-  .get(getProxy)
-  .put(updateProxy)
-  .delete(removeProxy);
+  /**
+   * @FIXME
+   */
+  function getProxy(req, res) {
+    notImplementedYet(res);
+  }
 
-module.exports = apiRouter;
+  /**
+   * @FIXME
+   */
+  function createProxy(req, res) {
+    notImplementedYet(res);
+  }
+
+  /**
+   * @FIXME
+   */
+  function updateProxy(req, res) {
+    notImplementedYet(res);
+  }
+
+  /**
+   * @FIXME
+   */
+  function removeProxy(req, res) {
+    notImplementedYet(res);
+  }
+
+  apiRouter.route('/').get(index);
+
+  apiRouter.route('/proxies')
+    .get(listProxies)
+    .post(createProxy);
+
+  apiRouter.route('/proxies/:id')
+    .get(getProxy)
+    .put(updateProxy)
+    .delete(removeProxy);
+
+  return apiRouter;
+})();
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,13 +1,13 @@
-'use strict';
+module.exports = (function () {
+  'use strict';
 
-var indexRouter = require('express').Router();
+  var indexRouter = require('express').Router();
 
-function index(req, res) {
-  res.render('index', { title: 'procKr' });
-}
+  function index(req, res) {
+    res.render('index', { title: 'procKr' });
+  }
 
-// register controllers
-indexRouter.route('/').get(index);
+  indexRouter.route('/').get(index);
 
-// exports router
-module.exports = indexRouter;
+  return indexRouter;
+})();
