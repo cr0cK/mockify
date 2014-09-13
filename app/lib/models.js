85ccce44672c3cec655e247039ad48fd31d713fe
Alexis Mineaud <alexis.mineaud@gmail.com>
2014-09-13 10:27:57 +0200

    Be strict only in the module implementation.

--- app/lib/models.js
+++ app/lib/models.js
@@ -1,59 +1,61 @@
-'use strict';
+module.exports = (function () {
+  'use strict';
 
-module.exports = {
-  /**
-   * Response model
-   */
-  Response: function (db) {
-    /* jscs:disable disallowSpaceAfterObjectKeys */
-    return db.define('response', {
-      uuid        : String,
-      dateCreated : Date,
-      status      : Number,
-      url         : String,
-      method      : String,
-      parameters  : Object,
-      reqHeaders  : Object,
-      resHeaders  : Object,
-      body        : Object,
-      comment     : String,
-      proxyId     : Number
+  return {
+    /**
+     * Response model
+     */
+    Response: function (db) {
+      /* jscs:disable disallowSpaceAfterObjectKeys */
+      return db.define('response', {
+        uuid        : String,
+        dateCreated : Date,
+        status      : Number,
+        url         : String,
+        method      : String,
+        parameters  : Object,
+        reqHeaders  : Object,
+        resHeaders  : Object,
+        body        : Object,
+        comment     : String,
+        proxyId     : Number
 
-    }, {
-      cache       : false,
-      methods     : {
-        // fullName: function () {
-        //   return this.name + ' ' + this.surname;
+      }, {
+        cache       : false,
+        methods     : {
+          // fullName: function () {
+          //   return this.name + ' ' + this.surname;
+          // }
+        }
+        // validations: {
+        //   age: orm.enforce.ranges.number(18, undefined, 'under-age')
         // }
-      }
-      // validations: {
-      //   age: orm.enforce.ranges.number(18, undefined, 'under-age')
-      // }
-    });
-    /* jscs:enable disallowSpaceAfterObjectKeys */
-  },
+      });
+      /* jscs:enable disallowSpaceAfterObjectKeys */
+    },
 
-  /**
-   * Proxy model
-   */
-  Proxy: function (db) {
-    /* jscs:disable disallowSpaceAfterObjectKeys */
-    return db.define('proxy', {
-      port        : Number,
-      target      : String,
-      isRecording : Boolean,
-      isEnabled  : Boolean
-    }, {
-      cache       : false,
-      methods     : {
-        // fullName: function () {
-        //   return this.name + ' ' + this.surname;
+    /**
+     * Proxy model
+     */
+    Proxy: function (db) {
+      /* jscs:disable disallowSpaceAfterObjectKeys */
+      return db.define('proxy', {
+        port        : Number,
+        target      : String,
+        isRecording : Boolean,
+        isEnabled  : Boolean
+      }, {
+        cache       : false,
+        methods     : {
+          // fullName: function () {
+          //   return this.name + ' ' + this.surname;
+          // }
+        }
+        // validations: {
+        //   age: orm.enforce.ranges.number(18, undefined, 'under-age')
         // }
-      }
-      // validations: {
-      //   age: orm.enforce.ranges.number(18, undefined, 'under-age')
-      // }
-    });
-    /* jscs:enable disallowSpaceAfterObjectKeys */
-  }
-};
+      });
+      /* jscs:enable disallowSpaceAfterObjectKeys */
+    }
+  };
+})();
