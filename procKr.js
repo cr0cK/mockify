0fe64a408b41a758da170de7ccd08cf8f9a2788f
Alexis Mineaud <alexis@gandi.net>
2014-09-17 19:34:24 +0200

    Some tests to display a nice ascii-table with proxies.

--- procKr.js
+++ procKr.js
@@ -5,6 +5,8 @@
 var fs        = require('fs'),
     program   = require('commander'),
     config    = require('./app/config'),
+    db        = require('./app/lib/db'),
+    proxy     = require('./app/entity/proxy'),
     log       = function () {
       console.log.apply(this, arguments);
     };
@@ -36,18 +38,55 @@ program
   .action(start);
 
 program
- .command('proxy-list')
- .description('List registered proxies.')
- .action(function () {
-   console.log('list proxies');
- });
+  .command('proxy-list')
+  .description('List registered proxies.')
+  .action(function () {
+    db.whenReady().then(function () {
+      proxy.list(function (err, proxies) {
+        // console.log(proxies);
+        //
+        // var io = require('socket.io');
+
+        // io.emit()
+
+        //
+        //
+        // var Table   = require('cli-table');
+        // var _       = require('./app/lib/helper')._;
+
+        // if (!proxies.length) {
+        //   return;
+        // }
+
+        // var table = new Table({
+        //   head: _.keys(_.publicProperties(_.first(proxies))),
+        //   style: {head: ['cyan']}
+        // });
+
+        // console.log(proxies[0]._isRecording);
+
+        // _.forEach(proxies, function (proxy) {
+        //   proxy = _.publicProperties(proxy);
+
+        //   var values = _.map(_.values(proxy), function (value) {
+        //     // console.log(value);
+        //     return value || false;
+        //   });
+
+        //   table.push(values);
+        // });
+
+        // log(table.toString());
+      });
+    });
+  });
 
 program.command('proxy-add <port> <target>')
- .description('Add a proxy which will listen on the port <port> and will ' +
+  .description('Add a proxy which will listen on the port <port> and will ' +
     'serve the target <target>.')
- .action(function () {
-   console.log('add proxy', arguments);
- });
+  .action(function () {
+    console.log('add proxy', arguments);
+  });
 
 program.parse(process.argv);
 
@@ -55,22 +94,3 @@ if (!program.args.length) {
   log( program.helpInformation() );
   process.exit();
 }
-
-
-// var Table = require('cli-table');
-
-// // instantiate
-// var table = new Table({
-//   head: ['port', 'target', 'rec.', 'status', 'id', 'enable'],
-//   style: {
-//     head: ['cyan']
-//   }
-// });
-
-// // table is an Array, so you can `push`, `unshift`, `splice` and friends
-// table.push(
-//   ['First value', 'Second value'],
-//   ['First value', 'Second value']
-// );
-
-// console.log(table.toString());
